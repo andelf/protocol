0d0bf8537e985ce09059ade3df0503124c4e51c5
Shuyu WANG <andelf@gmail.com>
2019-12-20 15:27:29 +0800

    feat: now prints recovery pub key

--- send.py
+++ send.py
@@ -46,15 +46,42 @@ payload = resp.json()
 
 pprint(payload)
 
+def signencode(r, s, order):
+    # order is stable for curves
+    print((r, s))
+
+    return r.to_bytes(32, 'big') + s.to_bytes(32, 'big')
 
 raw_data = bytes.fromhex(payload['raw_data_hex'])
-signature = priv_key.sign_deterministic(raw_data, hashfunc=hashlib.sha256)
+signature = priv_key.sign_deterministic(raw_data, hashfunc=hashlib.sha256, sigencode=signencode)
 
-print(signature.hex())
 
-payload['signature'] = [signature.hex() + '00']
+# calculate v?
+def verifying_key_to_addr(key):
+    pub_key = key.to_string()
+    primitive_addr = b'\x41' + keccak_256(pub_key)[-20:]
+    addr = base58.b58encode_check(primitive_addr)
+    return addr
 
-pprint(payload)
+pub_keys = ecdsa.VerifyingKey.from_public_key_recovery(
+    signature[:64], raw_data, curve=ecdsa.SECP256k1,
+    hashfunc=hashlib.sha256
+)
+
+print(pub_keys)
+
+for pk in pub_keys:
+    print(verifying_key_to_addr(pk))
+
+v = 1
+signature += bytes([v])
+
+print('signature =', signature.hex())
+
+
+payload['signature'] = [signature.hex()]
+
+# pprint(payload)
 
 print('=> broadcasttransaction')
 resp = requests.post('https://api.shasta.trongrid.io/wallet/broadcasttransaction', json=payload)
@@ -62,3 +89,5 @@ resp = requests.post('https://api.shasta.trongrid.io/wallet/broadcasttransaction
 result = resp.json()
 
 pprint(result)
+if 'message' in result:
+    print('MSG:', bytes.fromhex(result['message']))
